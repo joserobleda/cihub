d3073445550a20139fe0476abefdb059cd949b1e
pdelmoral <pdelmoral@dokify.net>
2013-02-06 10:44:46 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-
 	var assert = require("assert");
 
 
@@ -7,6 +6,7 @@
 			it('should return -1 when the value is not present', function(){
 				assert.equal(-1, [1,2,3].indexOf(5));
 				assert.equal(-1, [1,2,3].indexOf(0));
+				assert.equal(1,2);//Probando cuando falla test
 			});
 		})
 	});
